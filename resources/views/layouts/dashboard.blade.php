f46d88369caab334d71bf8c7f91766693844d919
Devin O'Connor <33325370+Dandyliun@users.noreply.github.com>
2018-03-08 21:18:14 -0500

    Started the sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,14 +3,21 @@
     @component('components.head')
     @endcomponent
     <body>
-        
-        <div id="topbar">
 
+        <div id="topbar">
+            <!--TODO: Add the PAWS Logo and the Search Bar-->
         </div>
 
-        <div uk-grid>
+        <div  uk-grid>
             <div id="sidebar" class="">
-
+                <ul class="uk-tab-right" uk-tab>
+                    <li class="uk-margin-small"><a href="#">Dashboard</a></li>
+                    <li class="uk-active uk-margin-small"><a href="#">Dogs</a></li>
+                    <li class="uk-margin-small"><a href="#">User</a></li>
+                    <li class="uk-margin-small"><a href="#">Profile</a></li>
+                    <li class="uk-margin-small"><a href="#">Email Log</a></li>
+                    <li class="uk-margin-small"><a href="#">Settings</a></li>
+                </ul>
             </div>
             <div id="content">
                 @yield('content')
